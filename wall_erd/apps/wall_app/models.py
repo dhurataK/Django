3a5a329623d5126c4baa1541a4f29998857bfef9
dhurataK <dhuratakamberi_@hotmail.com>
2016-11-10 16:14:50 +0100

    Wall_Erd assingment (Django Level II)

---
+++ wall_erd/apps/wall_app/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Message(models.Model):
+    user_id = models.ForeignKey(User)
+    message = models.TextField(max_length= 1000)
+    created_at = models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now =True)
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User)
+    message_id = models.ForeignKey(Message)
+    comment = models.TextField(max_length= 1000)
+    created_at = models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now =True)
